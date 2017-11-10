bc2889a2426362039c7a7bc9681eff33776364eb
yaniragl <yanira_galeas@hotmail.com>
2017-11-10 19:23:06 -0300

    onexion Base de Datos

--- src/condicionales/clasebd.java
+++ src/condicionales/clasebd.java
@@ -4,11 +4,60 @@
  * and open the template in the editor.
  */
 package condicionales;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-/**
- *
+/**Nombre yanira Galeas Rojas
+ *Fecha 10 11 2017
+ * Clase Prueba Java3
  * @author ceduc
+ * Responsabilidad Aplicacion de Escritorio Java
+ *
+ * 
  */
 public class clasebd {
+ private static Connection conex; 
+  private static final String DRIVER = "com.mysql.jdbc.Driver";
+  private static final String USER = "root";
+  private static final String PASS = "";
+  private static final String URL = "jdbc:mysql://localhost:330/clasebd";
+  {
+      conex = null; //Inicializar la variable
+     //Metodo para manejar excepciones 
+      try 
+  {
+      Class.forName(DRIVER);
+      conex =DriverManager.getConnection(URL, USER,PASS);
+      if(conex != null)
+  {
+         System.out.println("Conexion Establecida");
+   }
+  
+  }
+       catch(ClassNotFoundException |SQLException event)
+  { 
+       System.out.println("Error en la conexion");
+  }  
+  }
+       //Paso 13 -Definicion metodos para conectar y desconectar
+        public Connection conectar()
+        
+    {
+        return conex;//Retorna el objeto conex 
+           
+     }
+        public void desconectar()
+     {
+        conex=null;
+        if (conex==null)
+     {
+         System.out.println("Conexion Finalizada");
+                
+     }
+     }
     
-}
+     }
+
+    
+
--- src/conexionbd2.java
+++ src/conexionbd2.java
@@ -3,21 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pruebajava3;
 
 /**Nombre yanira Galeas Rojas
- * Fecha 10 11 2017
+ *Fecha 10 11 2017
  * Clase Prueba Java3
  * @author ceduc
  * Responsabilidad Aplicacion de Escritorio Java
+ *
  */
-public class PruebaJava3 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class conexionbd2 {
     
 }
